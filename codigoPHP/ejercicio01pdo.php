41da88ce722a16db2f7a592c2ce1ca30c9e43410
SrPozuelo <oscar.pozvil@educa.jcyl.es>
2026-03-18 13:58:00 +0100

    update ejercicios 01 y 02 y errores.

--- codigoPHP/ejercicio01pdo.php
+++ codigoPHP/ejercicio01pdo.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Tema 3 | Óscar Pozuelo Villamandos</title>
+        <title>Tema 4 | Óscar Pozuelo Villamandos</title>
         <link rel="stylesheet" href="../webroot/css/fonts.css">
         <link rel="stylesheet" href="../webroot/css/all.min.css">
         <link rel="stylesheet" href="../webroot/css/estilos.css"> 
@@ -19,91 +19,92 @@
                     <h1>Óscar Pozuelo Villamandos</h1>
                 </div>
                 <div class="curso-badge" style="background-color: #777BB4; color: white;">
-                    Tema 3
+                    Tema 4
                 </div>
             </div>
         </header>
         <main id="contenedor">
             <div id="titulo">1-Conexión a la base de datos con la cuenta usuario y tratamiento de errores..</div>
-             <?php
-       /**
-        * @author: Óscar Pozuelo
-        * @since: 17/03/2026
-        * 1. Conexión a la base de datos con la cuenta usuario y tratamiento de errores. Utilizar excepciones automáticas siempre que sea posible en todos los ejercicios.
-        */
-        // importamos el archivo con los datos de conexión
-        require_once '../conf/ConfDBPDO.php';
-
-        // Atributos de la conexión para usar después al mostrar
-        $aAtributos = array(
-            "AUTOCOMMIT",
-            "ERRMODE",
-            "CASE", "CLIENT_VERSION", "CONNECTION_STATUS",
-            "ORACLE_NULLS", "PERSISTENT", "PREFETCH", "SERVER_INFO", "SERVER_VERSION",
-            "TIMEOUT"
-        );
-
-        // Establecimiento de conexión con valores correctos
-        echo '<h3>Conexión a DBGJLDWESProyectoTema4 correctamente: </h3>';
-        try {
-            $miDB = new PDO(DSN,USERNAME,PASSWORD);
-            echo 'Conectado a la BBDD con éxito';
-            echo '<br><br>';
-
-            echo '<p><b>Atributos de la conexión: </b></p>';
-            foreach ( $aAtributos as $atributo ) {
-                echo "PDO::ATTR_$atributo: ";
-                try {
-                    echo '<span class="azul">'.$miDB->getAttribute( constant( "PDO::ATTR_$atributo" ) ) . "</span><br>";
-                } catch ( PDOException $miExceptionPDO ) {
-                    echo '<span class="rojo"> <b>Error: </b>'.$miExceptionPDO->getMessage().' <b>con código de error:</b> '.$miExceptionPDO->getCode()."</span><br>";
+            <?php
+                /**
+                 * @author: Óscar Pozuelo
+                 * @since: 17/03/2026
+                 * 1. Conexión a la base de datos con la cuenta usuario y tratamiento de errores. Utilizar excepciones automáticas siempre que sea posible en todos los ejercicios.
+                 */
+                // importamos el archivo con los datos de conexión
+                require_once '../conf/ConfDBPDO.php';
+                //Atributos de la conexión para usar después al mostrar
+                $aAtributos = array(
+                    "AUTOCOMMIT",
+                    "ERRMODE",
+                    "CASE",
+                    "CLIENT_VERSION",
+                    "CONNECTION_STATUS",
+                    "ORACLE_NULLS",
+                    "PERSISTENT",
+                    "PREFETCH",
+                    "SERVER_INFO",
+                    "SERVER_VERSION",
+                    "TIMEOUT"
+                );
+                // Establecimiento de conexión con valores correctos
+                echo '<h3>Conexión a DBOPVDWESProyectoTema4 correctamente: </h3>';
+                try{
+                    $miDB = new PDO(DSN,USERNAME,PASSWORD);
+                    echo '<p>Conectado a la BBDD con éxito</p>';
+                    echo '<h4>Atributos de la conexión:</h4>';
+                    foreach( $aAtributos as $atributo ){
+                        echo "<p><span class='variable'>PDO::ATTR_$atributo:</span>";
+                        try{
+                            echo '<span class="azul">'.$miDB->getAttribute(constant("PDO::ATTR_$atributo" ))."</span></p>";
+                        }
+                        catch ( PDOException $miExceptionPDO ) {
+                            echo '<span class="rojo"> <b>Error: </b>'.$miExceptionPDO->getMessage().'<b>con código de error:</b>'.$miExceptionPDO->getCode()."</span></p>";
+                        }
+                    }
                 }
-            }
-
-        } catch (PDOException $miExceptionPDO) {
-            echo 'Error: '.$miExceptionPDO->getMessage();
-            echo '<br>';
-            echo 'Código de error: '.$miExceptionPDO->getCode();
-        } finally {
-            unset($miDB);
-        }
-
-        // Establecimiento de conexión con valores incorrectos
-        echo '<h3>Conexión a DBGJLDWESProyectoTema4 incorrectamente: </h3>';
-        try {
-            $miDB = new PDO(DSN,USERNAME,'error');
-            echo 'Conectado a la BBDD con éxito';
-            echo '<br><br>';
-
-            echo '<p><b>Atributos de la conexión: </b></p>';
-            foreach ( $aAtributos as $atributo ) {
-                echo "PDO::ATTR_$atributo: ";
-                try {
-                    echo '<span class="azul">'.$miDB->getAttribute( constant( "PDO::ATTR_$atributo" ) ) . "</span><br>";
-                } catch ( PDOException $miExceptionPDO ) {
-                    echo '<span class="rojo"> <b>Error: </b>'.$miExceptionPDO->getMessage().' <b>con código de error:</b> '.$miExceptionPDO->getCode()."</span><br>";
+                catch (PDOException $miExceptionPDO){
+                    echo '<p>Error: '.$miExceptionPDO->getMessage().'</p>';
+                    echo '<p>Código de error: '.$miExceptionPDO->getCode().'</p>';
                 }
-            }
-
-        } catch (PDOException $miExceptionPDO) {
-            echo 'Error: '.$miExceptionPDO->getMessage();
-            echo '<br>';
-            echo 'Código de error: '.$miExceptionPDO->getCode();
-        } finally {
-            unset($miDB);
-        }
-        ?>
+                finally{
+                    unset($miDB);
+                }
+                // Establecimiento de conexión con valores incorrectos
+                echo '<h3>Conexión a DBOPVDWESProyectoTema4 incorrectamente: </h3>';
+                try{
+                    $miDB = new PDO(DSN,USERNAME,'error');
+                    echo '<p>Conectado a la BBDD con éxito</p>';
+                    echo '<p>Atributos de la conexión:</p>';
+                    foreach ( $aAtributos as $atributo ) {
+                        echo "<p><span class='variable' PDO::ATTR_$atributo:</span>";
+                        try {
+                            echo '<p><span class="azul">'.$miDB->getAttribute( constant( "PDO::ATTR_$atributo" ) ) . "</span></p>";
+                        }
+                        catch(PDOException $miExceptionPDO){
+                            echo '<p><span class="rojo"><b>Error:</b>'.$miExceptionPDO->getMessage().'<b>con código de error:</b>'.$miExceptionPDO->getCode()."</span></p>";
+                        }
+                    }
+                }
+                catch (PDOException $miExceptionPDO){
+                    echo '<p><span class="rojo">Error: '.$miExceptionPDO->getMessage().'</span></p>';
+                    echo '<p><span class="rojo">Código de error: '.$miExceptionPDO->getCode().'</span></p>';
+                }
+                finally{
+                    unset($miDB);
+                }
+            ?>
         </main>
         <footer class="pie-pagina">
             <div class="contenido-footer">
                 <div class="texto-legal">
                     <p>2025-26 IES LOS SAUCES. ©Todos los derechos reservados.</p>
-                    <p class="autor"><a href="https://oscarpozvil.ieslossauces.es" target="_blank">Óscar Pozuelo Villamandos.</a> Fecha de Actualización: 23-02-2026</p>
+                    <p class="autor"><a href="https://oscarpozvil.ieslossauces.es" target="_blank">Óscar Pozuelo Villamandos.</a> Fecha de Actualización: 18-03-2026</p>
                 </div>
                 <div class="iconos-footer">
-                    <a href="https://github.com/SrPozuelo/OPVDWESProyectoTema3" target="_blank" title="GitHub"><i class="fa-brands fa-github"></i></a>
-                    <a href="../indexProyectoTema3.html" title="Inicio"><i class="fa-solid fa-house"></i></a>
-                    <a href="../indexProyectoTema3.html" title="Volver a Tema3"><i class="fa-solid fa-arrow-turn-up"></i></a>
+                    <a href="https://github.com/SrPozuelo/OPVDWESProyectoTema4" target="_blank" title="GitHub"><i class="fa-brands fa-github"></i></a>
+                    <a href="../indexProyectoTema4.html" title="Inicio"><i class="fa-solid fa-house"></i></a>
+                    <a href="../indexProyectoTema4.html" title="Volver a Tema4"><i class="fa-solid fa-arrow-turn-up"></i></a>
                 </div>
             </div>
         </footer>
---
+++ codigoPHP/ejercicio02pdo.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tema 4 | Óscar Pozuelo Villamandos</title>
+        <link rel="stylesheet" href="../webroot/css/fonts.css">
+        <link rel="stylesheet" href="../webroot/css/all.min.css">
+        <link rel="stylesheet" href="../webroot/css/estilos.css"> 
+        <link rel="stylesheet" href="../webroot/css/estilosTabla.css"> 
+    </head>
+    <body>
+        <header class="cabecera-principal">
+            <div class="contenido-cabecera">
+                <div class="identidad">
+                    <a href="../index.html" style="text-decoration:none;">
+                        <div class="logo-iniciales">ÓS</div>
+                    </a>
+                    <h1>Óscar Pozuelo Villamandos</h1>
+                </div>
+                <div class="curso-badge" style="background-color: #777BB4; color: white;">
+                    Tema 4
+                </div>
+            </div>
+        </header>
+        <main id="contenedor">
+            <div id="titulo">2-Mostrar el contenido de la tabla Departamento y el número de registros..</div>
+            <?php
+                /**
+                * @author: Óscar Pozuelo
+                * @since: 18/03/2026
+                * 2. Mostrar el contenido de la tabla Departamento y el número de registros.
+                */
+                require_once '../conf/ConfDBPDO.php';
+                echo '<h3>Tabla usando consultas preparadas</h3>';
+                //variable para contar el numero de registros recuperados de la BBDD
+                $iNumRegistros=0;
+                try {
+                    $miDB=new PDO(DSN,USERNAME,PASSWORD);
+                    $sql="select * from T02_Departamento";
+                    $consulta=$miDB->prepare($sql);
+                    $consulta->execute();
+                    echo '<table>';
+                    echo '<tr>';
+                    echo '<th>Código▼</th>';
+                    echo '<th>Departamento</th>';
+                    echo '<th>Fecha de Creacion</th>';
+                    echo '<th>Volumen de Negocio</th>';
+                    echo '<th>Fecha de Baja</th>';
+                    echo '</tr>';
+                    while ($registro = $consulta->fetch()) {
+                        echo '<tr>';
+                        echo '<td>'.$registro['T02_CodDepartamento'].'</td>';
+                        echo '<td>'.$registro["T02_DescDepartamento"].'</td>';
+                        //construimos la fecha a partir de la que hay en la bbdd y luego mostramos sólo dia mes y año
+                        $oFecha=new DateTime($registro["T02_FechaCreacionDepartamento"]);
+                        echo '<td>'.$oFecha->format('d/m/Y').'</td>';
+                        //formateamos el float para que se vea en €
+                        echo '<td>'.number_format($registro["T02_VolumenDeNegocio"],2,',','.').' €</td>';
+                        if(is_null($registro["T02_FechaBajaDepartamento"])){
+                            echo '<td></td>';
+                        }
+                        else{
+                            $oFecha = new DateTime($registro["T02_FechaBajaDepartamento"]);
+                            echo '<td>'.$oFecha->format('d/m/Y').'</td>';
+                        }
+                        echo '</tr>';
+                        $iNumRegistros++;
+                    }
+                    echo '</table>';
+                    echo '<h3>Número de registros: '.$iNumRegistros.'</h3>';
+                }
+                catch(PDOException $miExceptionPDO) {
+                    echo 'Error: '.$miExceptionPDO->getMessage();
+                    echo '<br>';
+                    echo 'Código de error: '.$miExceptionPDO->getCode();
+                }
+                finally{
+                    unset($miDB);
+                }
+                echo '<h3>Tabla usando consultas con query</h3>';
+                try {
+                    $miDB=new PDO(DSN,USERNAME,PASSWORD);
+                    $sql="select * from T02_Departamento";
+                    // No se puede usar exec para consultas de select https://www.php.net/manual/es/pdo.exec.php
+                    // $numRegistros = $miDB->exec('select * from T02_Departamento');
+                    $iNumRegistros=0;
+                    $consulta=$miDB->query($sql);
+                    echo '<table>';
+                    echo '<tr>';
+                    echo '<th>Código▼</th>';
+                    echo '<th>Departamento</th>';
+                    echo '<th>Fecha de Creacion</th>';
+                    echo '<th>Volumen de Negocio</th>';
+                    echo '<th>Fecha de Baja</th>';
+                    echo '</tr>';
+                    while ($registro=$consulta->fetch()) {
+                        echo '<tr>';
+                        echo '<td>'.$registro['T02_CodDepartamento'].'</td>';
+                        echo '<td>'.$registro["T02_DescDepartamento"].'</td>';
+                        //construimos la fecha a partir de la que hay en la bbdd y luego mostramos sólo dia mes y año
+                        $oFecha=new DateTime($registro["T02_FechaCreacionDepartamento"]);
+                        echo '<td>'.$oFecha->format('d/m/Y').'</td>';
+                        //formateamos el float para que se vea en €
+                        echo '<td>'.number_format($registro["T02_VolumenDeNegocio"],2,',','.').' €</td>';
+                        if(is_null($registro["T02_FechaBajaDepartamento"])){
+                            echo '<td></td>';
+                        }
+                        else{
+                            $oFecha = new DateTime($registro["T02_FechaBajaDepartamento"]);
+                            echo '<td>'.$oFecha->format('d/m/Y').'</td>';
+                        }
+                        echo '</tr>';
+                        $iNumRegistros++;
+                    }
+                    echo '</table>';
+                    echo '<h3>Número de registros: '.$iNumRegistros.'</h3>';
+
+                } catch (PDOException $miExceptionPDO) {
+                    echo 'Error: '.$miExceptionPDO->getMessage();
+                    echo '<br>';
+                    echo 'Código de error: '.$miExceptionPDO->getCode();
+                } finally {
+                    unset($miDB);
+                }
+            ?>
+        </main>
+        <footer class="pie-pagina">
+            <div class="contenido-footer">
+                <div class="texto-legal">
+                    <p>2025-26 IES LOS SAUCES. ©Todos los derechos reservados.</p>
+                    <p class="autor"><a href="https://oscarpozvil.ieslossauces.es" target="_blank">Óscar Pozuelo Villamandos.</a> Fecha de Actualización: 18-03-2026</p>
+                </div>
+                <div class="iconos-footer">
+                    <a href="https://github.com/SrPozuelo/OPVDWESProyectoTema4" target="_blank" title="GitHub"><i class="fa-brands fa-github"></i></a>
+                    <a href="../indexProyectoTema4.html" title="Inicio"><i class="fa-solid fa-house"></i></a>
+                    <a href="../indexProyectoTema4.html" title="Volver a Tema4"><i class="fa-solid fa-arrow-turn-up"></i></a>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
