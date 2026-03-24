d48b596e6c187da78d2c234c62a346a79fe3c471
SrPozuelo <oscar.pozvil@educa.jcyl.es>
2026-03-24 12:26:13 +0100

    update ejercicio 05 pdo y archivo ConfDBPDO.

--- conf/ConfDBPDO.php
+++ conf/ConfDBPDO.php
@@ -1,6 +1,6 @@
 <?php 
     //https://www.hostinger.com/es/tutoriales/conectar-php-mysql
-    define('DSN', 'mysql:host=' . $_SERVER['SERVER_ADDR'] . '; dbname=DBOPVDWESProyectoTema4');
+    define('DSN', 'mysql:host=localhost; dbname=DBOPVDWESProyectoTema4');
     define('USERNAME','userOPVDWESProyectoTema4');
     define('PASSWORD','5813Libro-Puro');
 ?>
